ec47c17e02099a3a07a93b5a9f0391942c402d3c
Rottie#7658 <89132@glr.nl>
2023-05-30 21:25:07 +0200

    fix solved

--- Events/client/server.js
+++ Events/client/server.js
@@ -9,14 +9,19 @@ module.exports = {
     once: "true",
     execute(client) {
 
-      app.get('/status', (req, res) => { res.send( {status: 200, message: 'OK'}) });
+      app.get('/status', (req, res) => { res.json( {status: 200, message: 'OK'}) });
       server.listen(3000, () => {});
       
       function executeGetRequest() {
-        http.get(`${process.env.ServerUrl}`, (res) => {
-          let data = ''; res.on('data', (chunk) => {data += chunk;});
-        })
-      }
+        http.get(process.env.ServerUrl, (res) => {
+        let data = '';
+      
+        res.on('data', (chunk) => { data += chunk });
+        res.on('end', () => {
+          try {  } catch {}
+        });
+      }).on('error', (err) => {});
+    }
       
       executeGetRequest();
       setInterval(executeGetRequest, 5 * 60 * 1000);
